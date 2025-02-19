e8797ec49575880c48b08f5b39cfc26366b3804c
alvyWeb <alvy@npcoding.com>
2025-02-19 20:54:59 +0600

    Modify News JS

--- app/home/news/news.js
+++ app/home/news/news.js
@@ -5,68 +5,6 @@ import AtPennMessage from "./atpenn";
 
 
 const News = ({ newsData }) => {
-  // const { news, isLoading } = useNews() || {};
-
-  // const isLoading = false;
-  // const news = newsData;
-
-  // const staticArticles = [
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט כה חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     description: "לורם איפסום הוא כינוי לטקסט כה חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle554.jpg",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "4 דק’ קריאה",
-  //   },
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle491.png",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "לפני שעה",
-  //   },
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle491.png",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "לפני שעה",
-  //   },
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle491.png",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "לפני שעה",
-  //   },
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle491.png",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "לפני שעה",
-  //   },
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle491.png",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "לפני שעה",
-  //   },
-  //   {
-  //     title: "לורם איפסום הוא כינוי לטקסט חסר משמעות לחלוטין - הנקרא לפעמים",
-  //     image: "/home/news/Rectangle491.png",
-  //     url: "#",
-  //     source: "אדמין",
-  //     publishedAt: "לפני שעה",
-  //   },
-    
-  //   // Add more static articles as needed
-  // ];
-
-  // const articlesToRender = useStaticContent ? staticArticles : newsData?.news;
-
   return (
     <main className="news_app">
       {newsData?.isLoading ? ( 
