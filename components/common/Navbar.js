1c26b48fd9ccc6dccc02fad2a9776258e919cedb
alvyWeb <alvy@npcoding.com>
2025-02-20 02:26:24 +0600

    Update Navbar.js

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -5,61 +5,69 @@ import NavbarButtonHover from "./NavbarButtonHover";
 import "./nabvar.scss";
 
 const Navbar = ({ light }) => {
-  const { user, hashLink: hashNumber, onHashLink } = useContext(MainContext);
+  const { user } = useContext(MainContext);
   const authenticateUser = user?.data;
 
   const menus = [
     {
       name: "חדשות",
-      href: "/",
       hash: "",
       icon: `/NavIcons/news-white.svg`,
-      blackIcon: `/NavIcons/news-white.svg`,
       activeIcon: `/NavIcons/news-active.svg`,
     },
     {
       name: "הרשמה",
-      href: "/#0",
       hash: "#0",
       icon: `/NavIcons/world-white.svg`,
-      blackIcon: `/NavIcons/world-white.svg`,
       activeIcon: `/NavIcons/world-active.svg`,
     },
     {
       name: "לוח משחקים",
-      href: "/#1",
       hash: "#1",
       icon: `/NavIcons/game-white.svg`,
-      blackIcon: `/NavIcons/game-white.svg`,
       activeIcon: `/NavIcons/game-active.svg`,
     },
     {
       name: "הימורים",
-      href: "/#4",
-      hash: "#4",
+      hash: "#2",
       icon: `/NavIcons/bets-white.svg`,
-      blackIcon: `/NavIcons/bets-white.svg`,
       activeIcon: `/NavIcons/bets-active.svg`,
     },
     {
       name: "דירוגים",
-      href: "/#3",
       hash: "#3",
       icon: `/NavIcons/rank-white.svg`,
-      blackIcon: `/NavIcons/rank-white.svg`,
       activeIcon: `/NavIcons/rank-active.svg`,
     },
   ];
+
+  const handleNavClick = (hash) => {
+    if (window.location.hash !== hash) {
+      window.location.hash = hash;
+      setTimeout(() => {
+        window.location.reload(); // Refresh page after hash change
+      }, 100); // Small delay to allow hash update
+    }
+  };
 
   return (
     <div className="navbar-container">
       <div className="navbar">
         {menus?.map((menu) => (
-          <NavbarButtonHover key={menu?.name} menu={menu} light={light} />
+          <button
+            key={menu.name}
+            className="nav-menu"
+            onClick={() => handleNavClick(menu.hash)}
+          >
+            <div className="menu-img">
+              <img src={menu.icon} alt={menu.name} />
+            </div>
+            <p>{menu.name}</p>
+          </button>
         ))}
       </div>
     </div>
   );
 };
 
-export default Navbar;
+export default Navbar; 
