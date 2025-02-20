e7e9daefda342dfe729d8883b13bfe111571d704
alvyWeb <alvy@npcoding.com>
2025-02-21 01:50:27 +0600

    Update page.js

--- app/games/page.js
+++ app/games/page.js
@@ -1,5 +1,5 @@
 import ProtectedRoute from "@/components/common/ProtectedRoute";
-// import styles from "@/styles/pages/Home.module.scss";
+import styles from "@/styles/pages/Home.module.scss";
 import TabComponent from "../gamescomponents/TabComponent";
 export default function Home() {
   return (
