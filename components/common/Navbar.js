fb7575abbf6abd9cf5b04343889b1fa773a681a6
alvyWeb <alvy@npcoding.com>
2025-02-26 03:11:02 +0600

    Fixing Navbar

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -1,12 +1,17 @@
 "use client";
 import { MainContext } from "@/context";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import NavbarButtonHover from "./NavbarButtonHover";
 import "./nabvar.scss";
 
 const Navbar = ({ light }) => {
   const { user } = useContext(MainContext);
   const authenticateUser = user?.data;
+  const [activeHash, setActiveHash] = useState("");
+
+  useEffect(() => {
+    setActiveHash(window.location.hash || ""); // Set initial active hash
+  }, []);
 
   const menus = [
     {
@@ -53,18 +58,21 @@ const Navbar = ({ light }) => {
   return (
     <div className="navbar-container">
       <div className="navbar">
-        {menus?.map((menu) => (
+        {menus?.map((menu) => {
+          const isActive = activeHash === menu.hash;
+          return (
           <button
             key={menu.name}
-            className="nav-menu"
+            className={`nav-menu ${isActive ? "active" : ""}`}
             onClick={() => handleNavClick(menu.hash)}
           >
             <div className="menu-img">
-              <img src={menu.icon} alt={menu.name} />
+              <img src={isActive ? menu.activeIcon : menu.icon} alt={menu.name} />
             </div>
             <p>{menu.name}</p>
           </button>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
