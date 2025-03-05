28ca017a5d1521e7b3424645558ca15f7371300e
alvyWeb <alvy@npcoding.com>
2025-03-05 19:31:35 +0600

    Update

--- app/propyl/page.js
+++ app/propyl/page.js
@@ -281,21 +281,21 @@ const Propyl = ({ standings, setStandings }) => {
                   <img src="./flags/br.png" />
                 </div>
               </div>
-
-              <div className="lavel_name">
-                <p>נועם פן (פנתר)</p>
-                {/* SKILLBAR */}{" "}
-                <div class="container">
-                  <div className="_flex">
-                    <p>רמה 1</p>
-                    <p>26/100</p>
-                  </div>
-                  <div class="bar front advanced"></div>
+            </div>
+            <div className="lavel_name">
+              <p>נועם פן (פנתר)</p>
+              {/* SKILLBAR */}{" "}
+              <div class="container">
+                <div className="_flex">
+                  <p>רמה 1</p>
+                  <p>26/100</p>
                 </div>
-                {/* END */}
+                <div class="bar front advanced"></div>
               </div>
+              {/* END */}
             </div>
           </div>
+
         </div>
       </div>
     </main>
