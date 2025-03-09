2b5e9b7d785848a26fb64a343b867eb0849ff6c2
alvyWeb <alvy@npcoding.com>
2025-03-10 00:27:27 +0600

    Update page.js

--- app/enrollment/single/page.js
+++ app/enrollment/single/page.js
@@ -83,7 +83,7 @@ export default function SingleGame(props) {
             </span>
           </div>
           <div className="head_body_bottom">
-            <Link to="../double/page" className="d_games">זוגות</Link>
+            <Link href="../double/page" className="d_games">זוגות</Link>
             <p className="s_games">יחידים</p>
           </div>
         </div>
