fd12b3924f648e63f7cf7b95b2945947f72634c4
alvyWeb <alvy@npcoding.com>
2025-03-11 01:16:44 +0600

    Update page.js

--- app/propyl/page.js
+++ app/propyl/page.js
@@ -1001,7 +1001,7 @@ const Propyl = ({ standings, setStandings }) => {
           <div className="winlossgraph profileCommonO">
             <h2>מאזן נצחונות והפסדים</h2>
             <div class="barlist">
-              <div dir="rtl" style={{ width: "100%", height: "290" }}>
+              <div style={{ width: "100%", height: "290" }}>
                 <ResponsiveContainer width="100%" height="100%">
                   <BarChart
                     width={450}
