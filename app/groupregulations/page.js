61528deb835711bdab47043baa830c4b7d92635a
alvyWeb <alvy@npcoding.com>
2025-03-13 02:04:03 +0600

    Update page.js

--- app/groupregulations/page.js
+++ app/groupregulations/page.js
@@ -5,57 +5,58 @@ const groupregulations = ({ standings, setStandings }) => {
   const bodyData = [
     {
       bodyData:
-        "1. My health condition is normal, and allows a normal and healthy game for me and my friends.",
+        "1. מצבי הבריאותי תקין, ומאפשר משחק תקין ובריא עבורי ועבור חבריי.",
     },
     {
       bodyData:
-        "2. I will not have any claim against the team and/or any of its managers for any matter and thing related to games / injuries and medical matters / records / pitches or any other matter.",
+        "2. לא תהיה לי כל טענה כלפי הקבוצה ו/או מי ממנהליה עבור כל עניין ודבר שקשור במשחקים / פציעות ועניינים רפואיים / רישומים / מגרשים או כל עניין אחר.",
     },
     {
       bodyData:
-        "3. I know that the recommendation before every game is to warm up and stretch in an orderly manner.",
+        "3. ידוע לי כי ההמלצה לפני כל משחק היא לעשות חימום ומתיחות באופן מסודר.",
     },
     {
       bodyData:
-        "4. I know the rules of the team and the games, and it is clear to me that unnecessary arguments about In/Out during the game will deteriorate the quality of the game. I remember that! No game is decided by one point, and although sometimes it seems to me something certain, it may not be true. And so - whoever has the ball on his side - is the one who decides. If it is a couple, the determination will be made between both of them together.",
+        "4. חוקי הקבוצה והמשחקים ידועים לי, וברור לי כי ויכוחים מיותרים על In/Out במהלך המשחק ידרדרו את איכות המשחק. אני זוכר ש! אף משחק לא מוכרע על נקודה אחת, ולמרות שלפעמים לי נראה משהו מסוים, יכול להיות שזה לא נכון. ולכן - מי שהכדור בצד שלו - הוא זה שקובע. אם מדובר בזוג, הקביעה תיעשה בין שניהם יחד.",
     },
     {
-      bodyData: `5. Winning and losing rules:
+      bodyData: `5. כללי ניצחון והפסד:
 
-            - Regarding winning the entire game, it doesn't matter how many games each team won - only sets count.
-            - A game that will be played for at least an hour and a half, and the result will be a draw until that moment (1:1 or 2:2), will go to one of the following 2 options:`,
+            - לעניין ניצחון המשחק כולו, אין חשיבות כמה משחקונים כל קבוצה ניצחה - רק סטים נחשבים.
+            - משחק שישוחק לפחות שעה וחצי, ותוצאתו תהיה תיקו עד אותו רגע (1:1 או 2:2), ילך לאחת מ-2 האופציות הבאות:`,
     },
     {
       bodyData:
-        "A. Super tie break (tie breaker up to 10). After the tie breaker, if there is still light, you can continue playing but without entering results.",
+        "א. סופר טיי ברייק (שובר שוויון עד 10). לאחר שובר השוויון, אם עדיין יהיה אור, אפשר להמשיך לשחק אבל ללא הזנת תוצאות.",
     },
     {
-      bodyData: `B. Another decisive act. If you choose this option - you must not stop until you finish the campaign. A team that stopped choosing the game will be the team that retired, the score entry will be 6:0 for this set, and the other team will be the winner. Of course, with the exception of interruptions beyond our control such as rain, lights out, rain of missiles, earthquake, terrorist intrusion, alien intrusion, etc.
+      bodyData: `ב. מערכה נוספת מכריעה. אם בוחרים באופציה זו - אסור להפסיק עד שמסיימים את המערכה. קבוצה שהפסיקה מבחירתה את המשחק תהיה הקבוצה שפרשה, הזנת התוצאה תהיה 6:0 למערכה זו, והקבוצה השניה תהיה המנצחת. כמובן למעט הפסקות שלא בשליטתנו כגון גשם, כיבוי תאורה, גשם של טילים, רעידת אדמה, חדירת מחבלים, חדירת חייזרים וכו׳.
 
-            The choice of route (A or B) will be a unanimous decision of all. If there is no decision - it is decided by means of a lottery on the spot (rotation of the racket to the floor - W or M in the case of Wilson to determine a course). Whoever does not respect this decision - will be considered to have retired from the game.
+            בחירת המסלול (א׳ או ב׳) תהיה בהחלטת כולם פה אחד. במידה ואין החלטה - מחליטים באמצעות הגרלה במקום (סיבוב מחבט לריצפה - W או M במקרה של ווילסון לקביעת מסלול). מי שלא יכבד החלטה זו - ייחשב שפרש מהמשחק.
             
-            - A player who retires before the end of two hours of a game without the consent of all the players, his team will technically lose regardless of what the result was. If the retirement was in the middle of a set, 6:0 will be recorded against him and he will be marked with a retirement, which means the loss of the entire game. Each special case will be decided by the management of the group.
-            - After two hours of play, a player who wants to quit can. If the retirement is in the middle of a set - 6:0 will be recorded against him in the current set only.`,
+            - שחקן שפורש לפני תום שעתיים של משחק שלא בהסכמת כל השחקנים, קבוצתו תפסיד טכנית ללא קשר מה הייתה התוצאה. אם הפרישה הייתה באמצע סט, יירשם 6:0 נגדו ותסומן לו פרישה, שמשמעה הפסד המשחק כולו. כל מקרה מיוחד יוכרע ע״י הנהלת הקבוצה.
+
+            - לאחר שעתיים של משחק, שחקן שירצה לפרוש יוכל. אם הפרישה היא באמצע סט - יירשם 6:0 נגדו במערכה הנוכחית בלבד.`,
     },
     {
       bodyData:
-        "6. I know that commenting to my partner during a game can significantly lower our ability to play well together, so I will try to respect my partner even when we lose points.",
+        "6. ידוע לי שהערה לפרטנר שלי במהלך משחק עלולה להוריד משמעותית את היכולת שלנו לשחק טוב ביחד, ולכן אשתדל לכבד את הפרטנר שלי גם כשמפסידים נקודות.",
     },
     {
       bodyData:
-        "7. I am aware that any dangerous throwing of a racket, dangerous throwing of a ball and/or any use of verbal or physical violence on the courts, may result in sanctions against me such as suspension from games or from the WhatsApp groups or from the entire group for a certain period or permanently.",
+        "7. ידוע לי שכל זריקת מחבט בצורה מסוכנת, העפת כדור בצורה מסוכנת ו/או כל שימוש באלימות מילולית או פיזית במגרשים, עלול לגרור סנקציות נגדי כגון השהיה ממשחקים או מקבוצות הווטסאפ או מהקבוצה כולה לתקופה מסויימת או לצמיתות.",
     },
     {
       bodyData:
-        "8. I approve receiving automatic SMS or WhatsApp messages about group activity (registration, assignment, cancellations, results, etc.).",
+        "8. אני מאשר קבלת הודעות אוטומטיות בסמס או ווטסאפ על פעילות הקבוצה (רישום, שיבוץ, ביטולים, תוצאות וכו׳).",
     },
     {
       bodyData:
-        "9. Game price as of January 1, 2023 - NIS 45 for a doubles game.",
+        "9. מחיר משחק נכון ל1-ינואר 2023 - 45 ש״ח למשחק זוגות.",
     },
     {
       bodyData:
-        "The conditions of the group and its rules are determined by the management of the group only, and may change from time to time.",
+        "תנאי הקבוצה וחוקיה נקבעים ע"י הנהלת הקבוצה בלבד, ועלולים להשתנות מעת לעת.",
     },
   ];
   return (
@@ -68,12 +69,12 @@ const groupregulations = ({ standings, setStandings }) => {
           </div>
         ))}
         <div className="groupregulations_confirm">
-          <p>I confirm that I have read the entire regulation.</p>
+          <p>אני מאשר שקראתי את כל התקנון.</p>
           <input type="checkbox" />
         </div>
         <div className="groupregulations_button">
           <Link href="/news">
-            <button>Finish</button>
+            <button>סיים</button>
           </Link>
         </div>
       </div>
