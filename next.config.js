af533c64579b1a6220963a1f86909d7788e81d51
alvyWeb <alvy@npcoding.com>
2025-03-16 03:27:18 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-//module.exports = nextConfig
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: "/",
-          destination: "/home",
-          permanent: true, // Set to true for SEO-friendly 301 redirect
-        },
-      ];
-    },
-  };
+module.exports = nextConfig
