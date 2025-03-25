802dd4d3ae9f112e09123a318e712b4de184dead
alvyWeb <alvy@npcoding.com>
2025-03-26 00:43:49 +0600

    Update Layout.js

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Navbar from '../common/Navbar';
 import ProfileHeader from '../common/ProfileHeader';
 import styles from '../../styles/layouts/Layout.module.scss';
+import { motion } from 'framer-motion';
 
 const Layout = ({ children }) => {
   return (
+    <motion.div
+      key={location.key} // Key ensures a new animation on route change
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }} // Adjust the duration for the animation speed
+    >
     <div className={styles.container}>
       <ProfileHeader></ProfileHeader>
       <div>
@@ -12,6 +20,7 @@ const Layout = ({ children }) => {
       </div>
       <Navbar></Navbar>
     </div>
+    </motion.div>
   );
 };
 
