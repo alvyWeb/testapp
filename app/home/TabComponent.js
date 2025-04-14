ab311091a48f0f4e1586dfd2a0f261fd8afc4654
alvyWeb <alvy@npcoding.com>
2025-04-15 03:00:37 +0600

    Update

--- app/home/TabComponent.js
+++ app/home/TabComponent.js
@@ -1,43 +1,16 @@
 "use client";
+
 import ProtectedRoute from "@/components/common/ProtectedRoute";
-import useNews from "@/api/useNews";
-import useRanks from "@/api/useRanks";
-import useTournaments from "@/api/useTournaments";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
-import ATPRank from "./atp_rank/atprank";
 import News from "./news/news.js";
-import "./tab.scss";
+import Article from "./news/Article.js";
 import SystemMessage from "./news/system";
 
 const TabComponent = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const tab = searchParams.get("tab");
-
-  const [activeSection, setActiveSection] = useState(tab ? tab : "news");
-  const [activeRank, setActiveRank] = useState("atp");
-
-  const tournamentData = useTournaments();
-  const newsData = useNews() || {};
-  const rankData = useRanks({ rank: activeRank });
-
-  const handleNavClick = (section) => {
-    setActiveSection(section);
-    router.push(`/?tab=${section}`);
-  };
-
-  // scroll top top
-  // useEffect(() => {
-  //   window.scrollTo({
-  //     top: 0,
-  //   });
-  // }, [activeSection]);
-
   return (
     <ProtectedRoute>
       <div className="container">
         <div className="sectionActive">
+          <Article/>
           <News newsData={newsData} />
         </div>
         <SystemMessage />
